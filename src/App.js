d6d1436cb018706286ef1653c8c984b0d0a6af34
lukesherwood <luke.sherwood55@gmail.com>
2021-01-08 10:10:07 +1300

    sign up and sign in working with basic home page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import './App.css';
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import {connect} from 'react-redux'
+import "./App.css";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import {autoLogin, logUserOut} from './actions/userActions'
-import SignUp from './SignUp';
-import SignIn from './SignIn'
+import { autoLogin, logUserOut } from "./actions/userActions";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+import Home from "./Home";
 
-import 'react-notifications/lib/notifications.css';
-import { NotificationContainer } from 'react-notifications';
-import Container from 'react-bootstrap/Container'
+import "react-notifications/lib/notifications.css";
+import { NotificationContainer } from "react-notifications";
+import Container from "react-bootstrap/Container";
 
 class App extends Component {
-  render () {
+  componentDidMount() {
+    this.props.autoLogin();
+  }
+  render() {
     return (
-    <div className="App">
-          <Container fluid>
-            <Switch>
-              <Route exact path='/signIn' component={SignIn}/>
-              <Route exact path='/signUp' component={SignUp}/>
-            </Switch>
-            <NotificationContainer />
-          </Container>
-    </div>
-  );
-}
+      <div className="App">
+        <Container fluid>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signIn" component={SignIn} />
+            <Route exact path="/signUp" component={SignUp} />
+          </Switch>
+          <NotificationContainer />
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.users.loggedIn,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    autoLogin: () => dispatch(autoLogin()),
+    logUserOut: () => dispatch(logUserOut()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import store from './store.js';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
    <Provider store={store}>
     <Router>
       <App />
     </Router>
   </Provider>,
-  </React.StrictMode>,
   document.getElementById('root')
 );
 
