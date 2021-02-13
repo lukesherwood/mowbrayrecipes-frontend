9e3ca7bb81b48afc23149f6a6ca5019d23aa6f46
lukesherwood <luke.sherwood55@gmail.com>
2021-02-13 11:16:45 -0500

    updated store to include redux devtools if dev

--- src/store.js
+++ src/store.js
@@ -1,17 +1,19 @@
-import {
-    createStore, 
-    applyMiddleware,
-    combineReducers
-  } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 
-  import thunk from 'redux-thunk';  
-  import users from './reducers/usersReducer'
-  import recipes from './reducers/recipesReducer'
-  
-  const reducers = combineReducers({
-    users,
-    recipes
-  });
-  
-  
-  export default createStore(reducers, applyMiddleware(thunk));
+import thunk from "redux-thunk";
+import users from "./reducers/usersReducer";
+import recipes from "./reducers/recipesReducer";
+
+const reducers = combineReducers({
+  users,
+  recipes,
+});
+let middleware = [thunk];
+let enhancers = [applyMiddleware(...middleware)];
+if (process.env.NODE_ENV === "development") {
+  enhancers.push(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
+
+export default createStore(reducers, compose(...enhancers));
