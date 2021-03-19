961cf12396926669b8df375357ce4adf31bfaf6f
lukesherwood <luke.sherwood55@gmail.com>
2021-03-19 17:09:46 -0400

    updating login actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { NotificationManager } from 'react-notifications';
-import { config } from '../Constants' 
-const WEB_URL = config.url.API_URL
+import { NotificationManager } from "react-notifications";
+import { config } from "../Constants";
+const WEB_URL = config.url.API_URL;
 
 // const WEB_URL = "https://mowbrayrecipes.herokuapp.com/api/v1/"
 
@@ -9,30 +9,36 @@ const setUser = (payload) => ({ type: "SET_USER", payload });
 
 export const logUserOut = () => ({ type: "LOG_OUT" });
 
-export const signUserUp = (userInfo) => (dispatch) => {
+export const signUserUp = (userInfo, ownProps) => (dispatch) => {
   axios
-    .post(
-      WEB_URL+"/users",
-      { user: userInfo },
-      { withCredentials: true }
-    )
-    .then((data) => { // can we create a method as its duplicated below
+    .post(WEB_URL + "/users", { user: userInfo }, { withCredentials: true })
+    .then((data) => {
+      // can we create a method as its duplicated below
       const authHeader = data.headers.authorization;
       if (authHeader.startsWith("Bearer ")) {
         const token = authHeader.substring(7, authHeader.length);
         localStorage.setItem("token", token);
         dispatch(setUser(data.data));
-        NotificationManager.success(`You have successfully signed up ${data.data.name}`, 'Successful!', 2000)
+        ownProps.history.push(`/`);
+        NotificationManager.success(
+          `You have successfully signed up ${data.data.name}`,
+          "Successful!",
+          2000
+        );
       }
     })
     .catch((error) =>
-    NotificationManager.error(`Error while Creating new user!, ${error}`, 'Error!'));
+      NotificationManager.error(
+        `Error while Creating new user!, ${error}`,
+        "Error!"
+      )
+    );
 };
 
-export const fetchUser = (userInfo) => (dispatch) => {
+export const fetchUser = (userInfo, ownProps) => (dispatch) => {
   axios
     .post(
-      WEB_URL+"/users/sign_in",
+      WEB_URL + "/users/sign_in",
       { user: userInfo },
       { withCredentials: true }
     )
@@ -42,32 +48,48 @@ export const fetchUser = (userInfo) => (dispatch) => {
         const token = authHeader.substring(7, authHeader.length);
         localStorage.setItem("token", token);
         dispatch(setUser(data.data));
-        NotificationManager.success(`You have successfully logged in ${data.data.name}`, 'Successful!', 2000)
+        ownProps.history.push(`/`);
+        NotificationManager.success(
+          `You have successfully logged in ${data.data.name}`,
+          "Successful!",
+          2000
+        );
       }
     })
-    .catch((error) => NotificationManager.error(`Error while signing in!, ${error}`, 'Error!'));
+    .catch((error) =>
+      NotificationManager.error(`Error while signing in!, ${error}`, "Error!")
+    );
 };
 
 export const autoLogin = () => (dispatch) => {
-    const token = localStorage.token;
-    if (token) {
-      axios.get(WEB_URL+"/users/auto_login", {
+  const token = localStorage.token;
+  if (token) {
+    axios
+      .get(WEB_URL + "/users/auto_login", {
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
           Authorization: `Bearer ${token}`,
         },
       })
-        .then((data) => {
-          if (!data.data.data) { // something goes wrong here
-            NotificationManager.error(`Error while signing in! ${data.message}`, 'Error!')
-            localStorage.removeItem("token");
-          } else {
-            dispatch(setUser(data.data));
-            NotificationManager.success(`Welcome back, ${data.data.data.attributes.name}`, 'Successful!', 2000)
-          }
-        });
-    }
-  };
-
-
+      .then((data) => {
+        if (!data.data.data) {
+          // something goes wrong here
+          NotificationManager.error(
+            `Error while signing in! ${data.message}`,
+            "Error!"
+          );
+          localStorage.removeItem("token");
+        } else if (data.data.id) {
+          dispatch(setUser(data.data));
+        }
+      })
+      .catch((error) => {
+        NotificationManager.error(
+          `Error while signing in, please try again`,
+          "Error!"
+        );
+        localStorage.removeItem("token");
+      });
+  }
+};
