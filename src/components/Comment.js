e08b8782c1cfd37fdb4449a20d2c15fedcc98a46
lukesherwood <luke.sherwood55@gmail.com>
2021-03-20 14:20:46 -0400

    mobile responsibility added to comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import moment from "moment";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Comment(props) {
     const { comment } = props;
     return (
         <div className="comment-card">
-            <div className="row">
-                <h5 className="col-1">{comment.name}</h5>
-            <div className="blockquote-footer col-4">{moment(comment.updated_at).format(
-                  "MMMM Do YYYY, h:mm a"
-                )}
-            </div>
-                <div className="col-2">{comment.rating} stars</div>
-            </div>
-
+            <Row sm={1} >
+                <Col sm={1}>{comment.name}</Col>
+                <Col md={4} className="blockquote-footer">{moment(comment.updated_at).format(
+                    "MMMM Do YYYY, h:mm a"
+                    )}
+                </Col>
+                <Col md={2}>{comment.rating} stars</Col>
+            </Row>
+            <br></br>
             <p>{comment.content}</p>
         </div>
     )
