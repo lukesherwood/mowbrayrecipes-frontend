54ea5d3febab2dc0063778187648690283dea492
lukesherwood <luke.sherwood55@gmail.com>
2021-03-27 11:41:51 -0400

    added icons

--- src/components/ModalRecipes.js
+++ src/components/ModalRecipes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 import Recipe from "./Recipe";
+import * as Icon from 'react-bootstrap-icons';
 
 export default function ModalRecipes(props) {
   let recipes = props.recipes || [];
@@ -12,13 +12,14 @@ export default function ModalRecipes(props) {
   return (
     <Modal {...props} size="md" centered>
       <Modal.Body>
-        <h3>Results for '{props.keyword.keyword}'</h3>
+        
+        <h3>
+          Results for '{props.keyword.keyword}'
+          <Icon.XSquare as="button" className="btn-danger float-right" onClick={props.onHide}/>
+        </h3>
         {recipeList}
         {recipes.length === 0 ? "Sorry no recipes found" : null}
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
     </Modal>
   );
 }
